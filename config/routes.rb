cae26bb779cc12a3ffaa31d64cfcef963a004b5e
mamrey <mamrey@upenn.edu>
2022-12-13 12:18:37 -0500

    Testing git user

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
 
-  # nested resource
+  # nested resources
 
   resources :articles do
     resources :comments
